92f8bb8cd10c6878d38e8e4a278670d16a689863
HAR15 <haris005@gmail.com>
2018-08-28 00:06:02 -0400

    v1 of Portfolio done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  aboutText: string = "I have been captivated by front-end development since I started in a junior web developer role at my university in 2013. Being able to create something beautiful from something mostly technical is what truly amazes me. I have worked on several web development projects and jobs ever since to own the joy of learning new things and making things look good. When I am not coding, I like listening to podcasts (Syntax.fm is awesome), playing basketball, ping-pong, and spending time with loved ones.  "
+  aboutText: object = {
+    background: "I have been captivated by front-end development since I started in a junior web developer role at my university in 2013. Being able to create something beautiful from something mostly technical is what truly amazes me. I have worked on several web development projects and jobs ever since to own the joy of learning new things and making things look good. ",
+    hobbies: "When I am not coding, I like listening to podcasts (Syntax.fm is awesome), playing basketball, ping-pong, and spending time with loved ones."
+  }
+  
   aboutPic: string = "../assets/HarisPic.png"
 
   constructor() { }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,19 +12,33 @@ export class ProjectsComponent implements OnInit {
       title: 'What2Watch',
       description: 'React.js web app that dynamically updates a list of movie results based on selected filters such as year, genre, IMDb rating, and Rotten Tomatoes rating. Each result shows more movie details.',
       image: '../assets/What2WatchPic.png',
-      url: 'https://github.com/HaR15/what2watch',
+      url: 'https://github.com/HaR15/what2watch',      
+      role: '',
+      stack: ''
     },
     {
       title: 'Minimalistic Breakout',
       description: 'A minimalistic version of the classic "Breakout" game built using HTML5 canvas and JavaScript.',
       image: '../assets/MinimalisticBreakout.png',
       url: 'https://github.com/HaR15/Minimalistic-Breakout',
+      role: '',
+      stack: ''
     },
     {
       title: 'DevSideProjects',
       description: 'Web app allowing project creators and developers to search and match based on preferences.',
-      image: '../assets/MinimalisticBreakout.png',
+      image: '../assets/DevSideProjects.png',
       url: 'https://github.com/KathyTheTurtle/DevSideProjects',
+      role: 'Implemented the front-end for the Developer Search and Project Search features.',
+      stack: ''
+    },
+    {
+      title: 'Baseball Cards E-store',
+      description: 'An e-commerce store for viewing and purchasing baseball cards in-stock.',
+      image: '../assets/BaseballEstore.png',
+      url: 'https://github.com/HaR15/estore',
+      role: 'Implemented admin operations for inventory management, items list, shopping cart, checkout.',
+      stack: ''
     },
   ];
 
